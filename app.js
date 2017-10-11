ec32cd4d583165fe85d6fc872b5a3392271cadbc
ayako1 <whitesa11@hotmail.co.jp>
2017-10-11 14:42:42 -0700

    add a new file

---
+++ app.js
@@ -0,0 +1,10 @@
+document.write("Hello");
+
+// Q1.
+function makeAbba(a, b) {
+	// body 
+}
+
+document.write(makeAbba("Hi", "Bye")); // "HiByeByeHi"
+document.write(makeAbba("Yo", "Alice")); // "YoAliceAliceYo"
+document.write(makeAbba("What", "Up")); // "WhatUpUpWhat"
