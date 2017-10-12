acdea88895c891d9cc7959beb73d6d4bd1f97122
ayako1 <whitesa11@hotmail.co.jp>
2017-10-12 01:38:18 -0700

    wrote the code

--- app.js
+++ app.js
@@ -36,11 +36,10 @@ document.write(firstHalf("abcdef"));
 document.write('<br>');
 
 // Q4.
-function right2(c){
- var j = c.slice (-2);
- var k = c.slice(1);
- var str = c.replace(j, k);
- 	return str
+function right2(z){
+ var j = z.slice(-2);
+ var k = z.slice(0 , -2);
+ 	return j + k
 }
 document.write(right2("Hello"));
 document.write(right2("java"));
@@ -49,43 +48,93 @@ document.write(right2("Hi"));
 document.write('<br>');
 
 // Q5.
-function conCat(c){
- var c = split [-2]
- 	c == "ly"
+function conCat(str1,str2){
+ 	if (str1.slice(-1) === str2.charAt(0)){
+ 		return str1 + str2.slice(1);
+ 	}else{ 
+ 		return str1.concat(str2);
+ 	}
 }
-document.write(endsLy("oddly"));
+document.write(conCat("abc" , "cat"));
+document.write(conCat("dog" , "cat"));
+document.write(conCat("abc" , ""));
+
+document.write('<br>');
 
 // Q6.
-function frontAgain(c){
- var c = split [-2]
- 	c == "ly"
+function frontAgain(str){
+ return str.slice(0,2) == str.slice(-2);
 }
-document.write(endsLy("oddly"));
+
+document.write(frontAgain("edited"));
+document.write(frontAgain("edit"));
+document.write(frontAgain("ed"));
+
+document.write('<br>');
 
 // Q7.
-function deFront(c){
- var c = split [-2]
- 	c == "ly"
+function deFront(str){
+	if(str.charAt(0) === "a"){
+		var j = str.slice(2)
+		return "a" + j ;
+	}else if(str.charAt(0) === "b"){
+		return str.slice(1);
+	}else{
+		return str.slice(2);
+	}
 }
-document.write(endsLy("oddly"));
+document.write(deFront("Hello"));
+document.write(deFront("java"));
+document.write(deFront("away"));
+document.write(deFront("battery"));
+
+document.write('<br>');
 
 // Q8.
-function withoutX2(c){
- var c = split [-2]
- 	c == "ly"
+function withoutX2(str){
+ if(str.charAt(0) == "x" && str.charAt(1) == "x"){
+ 	return str.slice(2);
+ }else if(str.charAt(0) == "x"){
+ 	return str.slice(1);
+ }else if(str.charAt(1) == "x"){
+ 	var j = str.slice(0,1);
+ 	return j + str.slice(2);
+ }else{
+ 	return str.slice(0);
+ }
 }
-document.write(endsLy("oddly"));
+document.write(withoutX2("xHi"));
+document.write(withoutX2("Hxi"));
+document.write(withoutX2("Hi"));
+document.write(withoutX2("xxHi"));
+
+document.write('<br>');
 
 // Q9.
-function lastChars(c){
- var c = split [-2]
- 	c == "ly"
+function lastChars(a,b){
+	if(a.length == 0){
+		return "@" + b.slice(-1);
+	}else if(b.length == 0){
+		return a.charAt(0) + "@";
+	}else{
+		return a.charAt(0) + b.slice(-1);
+	}
 }
-document.write(endsLy("oddly"));
+document.write(lastChars("last", "chars"));
+document.write(lastChars("yo", "java"));
+document.write(lastChars("hi", ""));
+
+document.write('<br>');
 
 // 10.
-function middleTwo(c){
- var c = split [-2]
- 	c == "ly"
+function middleTwo(str){
+	if (str.length >= 2 && str.length % 2 == 0){
+		var x = str.length / 2;
+		var y = str.slice(0 , x);
+		var z = str.slice(x);
+		return y.slice(-1) + z.charAt(0);
+	}
 }
-document.write(endsLy("oddly"));
+document.write(middleTwo("string"));
+document.write(middleTwo("code"));
+document.write(middleTwo("Practice"));
